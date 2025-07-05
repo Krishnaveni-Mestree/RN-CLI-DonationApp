cc1897b39f6110207f56d4418791cb5bd33dc898
Krishnaveni-Mestree <mestreekrishnaveni@gmail.com>
2025-07-05 19:58:07 +0530

    Installed FontAwesome Icons and tested with faSearch icon

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -7,6 +7,8 @@ import Header from "../../components/Header/Header";
 import Button from "../../components/Button/Button";
 import Tab from "../../components/TabButton/Tab";
 import Badge from "../../components/Badge/Badge";
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Home=()=>{
     return(
@@ -19,6 +21,7 @@ const Home=()=>{
                 <Tab title={"Highlight-Lifestyle"} />
                 <Tab title={"Highlight"} isInactive={true}/>
                 <Badge title={"Environment"}/>
+                <FontAwesomeIcon icon={faSearch}/>
             </View>
         </SafeAreaView>
     );
